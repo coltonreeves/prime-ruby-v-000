324da46af09f9d269fb96c8209d1b1da5522917f
coltonreeves <coltonallenreeves@gmail.com>
2017-10-11 11:37:52 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def sieve(integer)
+  primes = (0..integer).to_a
+  primes[0] = primes[1] = nil
+  counter = 0
+  primes.each do |p|
+    next unless p
+    break if p*p > integer
+    counter += 1
+    (p*p).step(integer,p) {|m| primes[m] = nil}
+  end
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
